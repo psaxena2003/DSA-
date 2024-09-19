552b16dac7b6b9146c1f816bb6e9710170a8941f
PRIYANSHI SAXENA <priyanshi200319saxena@gmail.com>
2024-09-20 01:33:17 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (17.93%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void sortColors(int[] nums) {
+       int start=0;
+       int end= nums.length-1;
+        int mid=0;
+        while(mid<=end){
+            switch(nums[mid]){
+                case 0:
+                    swap(nums, start, mid);
+                    start++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(nums, mid, end);
+                    end--;
+                    break;
+            }
+        }
+    }
+    public void swap(int[] arr, int pos1, int pos2){
+        int temp=0;
+        temp = arr[pos1];
+        arr[pos1]=arr[pos2];
+        arr[pos2]=temp;
+    }
+}
